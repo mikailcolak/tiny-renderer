173def72ba91b152ce65d240a41d926bdf662c57
Mikail ÇOLAK <mikailcolak@gmail.com>
2022-09-12 04:26:25 +0200

    constant variables moved out from loop.

--- lessons/perspective_projection.cpp
+++ lessons/perspective_projection.cpp
@@ -9,36 +9,35 @@
 #include <array>
 
 void perspective_projection_study_1(TGAImage& image) {
+  const glm::vec2 scale{ 0.25f, 0.25f };
+  const glm::vec2 translation{ 0.7f, 0.7f };
+  const glm::vec2 rotation{ glm::radians(10.0f), glm::radians(10.0f)};
+
+  const float c2 = glm::cos(rotation.x);
+  const float s2 = glm::sin(rotation.x);
+  const float c1 = glm::cos(rotation.y);
+  const float s1 = glm::sin(rotation.y);
+
+  glm::mat3 model_transformation_matrix(
+    {
+      scale.x * c1             ,
+      scale.x * -s1            ,
+      0.0f                     ,
+    },
+    {
+      scale.y * s2,
+      scale.y * c2             ,
+      0.0f                     ,
+    },
+    {
+      translation.x            ,
+      translation.y            ,
+      1.0f                     ,
+    }
+  );
+
   load_model_and_for_each_face("assets/plane.obj", [&](auto face_vertices, auto face_normals, auto face_texcoords) -> void {
     std::array<glm::vec2, 3> screen_coords;
-
-    const glm::vec2 scale{ 0.25f, 0.25f };
-    const glm::vec2 translation{ 0.7f, 0.7f };
-    const glm::vec2 rotation{ glm::radians(10.0f), glm::radians(10.0f)};
-
-    const float c2 = glm::cos(rotation.x);
-    const float s2 = glm::sin(rotation.x);
-    const float c1 = glm::cos(rotation.y);
-    const float s1 = glm::sin(rotation.y);
-
-    glm::mat3 model_transformation_matrix(
-      {
-        scale.x * c1,
-        scale.x * -s1           ,
-        0.0f                    ,
-      },
-      {
-        scale.y * s2,
-        scale.y * c2             ,
-        0.0f                     ,
-      },
-      {
-        translation.x            ,
-        translation.y            ,
-        1.0f                     ,
-      }
-    );
-
     std::transform(face_vertices.begin(), face_vertices.end(), screen_coords.begin(), [&](auto& p) {
       return glm::vec2{
         world_to_screen(
